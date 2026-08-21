25eb18598cd763c34d4ed24494afd08785acd990
MinimaxFlora <zj18139624826@gmail.com>
2026-08-22 02:08:47 +0800

    base-files: set default root password
    Set the default root password in /etc/shadow by generating an MD5-crypt password hash during the build.
    
    This prevents the default root password from being left unset or unchanged.

--- diy-script.sh
+++ diy-script.sh
@@ -6,6 +6,10 @@ sed -i 's/ImmortalWrt/ZeroWrt/g' package/base-files/files/bin/config_generate
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
+# 修改默认密码
+default_password=$(openssl passwd -5 password)
+sed -i "s|^root:[^:]*:|root:${default_password}:|" package/base-files/files/etc/shadow
+
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
